f0ab593caab11198f2362cea704f7387e6e20acc
Viviane Martini <martini.viviane@gmail.com>
2023-10-20 13:25:02 -0300

    finish details area

--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -37,7 +37,19 @@ export const Content = styled.div`
     flex-direction: column;
 
     > button:first-child {
-        
+        align-self: end;
+    }
+
+    > h1 {
+        font-size: 36px;
+        font-weight: 500;
+        padding-top: 64px;
+    }
+
+    > p {
+        font-size: 16px;
+        margin-top: 16px;
+        text-align: justify;
     }
 
 `
