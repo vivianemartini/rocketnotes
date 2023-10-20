1eeea2282d798b74d23de5010a1f18b6d1b5dbb3
Viviane Martini <martini.viviane@gmail.com>
2023-10-20 11:33:17 -0300

    feat: add btn text

---
+++ src/components/ButtonText/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Container = styled.button`
+    border: none;
+    color: ${({ theme }) => theme.COLORS.ORANGE};
+    background: none;
+    font-size: 16px;
+`
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -5,33 +5,38 @@ import { Button } from '../../components/Button'
 import { Header } from '../../components/Header'
 import { Section } from '../../components/Section'
 import { Tag } from '../../components/Tag'
-
+import { ButtonText } from '../../components/ButtonText'
 
 export function Details() {
   return (
     <Fragment>
-     <Container>
+      <Container>
         <Header />
 
-        <Section title="Links Utéis">
-        {/* Children passa no meio do componente não como propriedade */}
+        <ButtonText title='Excluir nota' />
+
+        <Section title='Links Utéis'>
+          {/* Children passa no meio do componente não como propriedade */}
           <Links>
-              <li>
-                <a href="https://www.rocketseat.com.br/">https://www.rocketseat.com.br/</a>
-              </li>
-              <li>
-                <a href="https://www.rocketseat.com.br/">https://www.rocketseat.com.br/</a>
-              </li>
+            <li>
+              <a href='https://www.rocketseat.com.br/'>
+                https://www.rocketseat.com.br/
+              </a>
+            </li>
+            <li>
+              <a href='https://www.rocketseat.com.br/'>
+                https://www.rocketseat.com.br/
+              </a>
+            </li>
           </Links>
         </Section>
 
-        <Section title="Marcadores">
-            <Tag title="express"/>
-            <Tag title="nodejs"/>
+        <Section title='Marcadores'>
+          <Tag title='express' />
+          <Tag title='nodejs' />
         </Section>
 
-        <Button title="Voltar"/>
-
+        <Button title='Voltar' />
       </Container>
     </Fragment>
   )
