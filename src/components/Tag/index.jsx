0d4db6953b42030930420dbc18913f54d55a0902
Viviane Martini <martini.viviane@gmail.com>
2023-10-20 11:27:58 -0300

    feat: add tag

---
+++ src/components/Tag/index.jsx
@@ -0,0 +1,10 @@
+import { Container } from './styles';
+
+// eslint-disable-next-line react/prop-types
+export function Tag({ title }){
+    return (
+        <Container>
+            {title}
+        </Container>
+    )
+}
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -3,7 +3,9 @@ import { Container, Links } from './styles'
 
 import { Button } from '../../components/Button'
 import { Header } from '../../components/Header'
-import { Section} from '../../components/Section'
+import { Section } from '../../components/Section'
+import { Tag } from '../../components/Tag'
+
 
 export function Details() {
   return (
@@ -21,6 +23,11 @@ export function Details() {
                 <a href="https://www.rocketseat.com.br/">https://www.rocketseat.com.br/</a>
               </li>
           </Links>
+        </Section>
+
+        <Section title="Marcadores">
+            <Tag title="express"/>
+            <Tag title="nodejs"/>
         </Section>
 
         <Button title="Voltar"/>
