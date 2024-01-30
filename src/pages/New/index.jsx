513e0d28366e1685ffa6da39f7631240078f4d43
Viviane Martini <martini.viviane@gmail.com>
2024-01-29 21:41:10 -0300

    finalizando o new component

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -3,6 +3,7 @@ import { Input } from '../../components/Input';
 import { Textarea } from '../../components/Textarea';
 import { NoteItem } from '../../components/NoteItem';
 import { Section } from '../../components/Section';
+import { Button } from '../../components/Button';
 
 import { Container, Form } from './styles';
 
@@ -26,6 +27,15 @@ export function New() {
                   <NoteItem value='https://google.com' />
                   <NoteItem isNew placeholder='Novo link' />
                 </Section>
+
+                <Section title="Marcadores">
+                  <div className='tags'>
+                    <NoteItem value='react' />
+                    <NoteItem isNew placeholder='Nova tag' />
+                  </div>
+                </Section>
+
+                <Button title="Salvar" />
             </Form>
         </main>
       </Container>
